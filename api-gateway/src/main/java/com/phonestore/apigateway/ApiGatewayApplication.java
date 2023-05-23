d6e6583ae001101fc37a3d674bac560194e65c75
snifee <razyaarf@gmail.com>
2023-05-24 03:35:57 +0800

    add api gateway

---
+++ api-gateway/src/main/java/com/phonestore/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.phonestore.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+
+        System.out.println("API Gateway Running");
+        System.out.println();
+    }
+}
